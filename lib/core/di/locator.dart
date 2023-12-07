b72d5ff3a46e5a72040c63368dbdd8793d2f92a6
Mariugo Megiani <mariugomegiani@gmail.com>
2023-12-07 18:58:04 -0300

    Add dependency injection for CryptoRepository, GetCoinsUsecase, and HomeController

--- lib/core/di/locator.dart
+++ lib/core/di/locator.dart
@@ -1,3 +1,8 @@
+import 'package:cryptos/app/features/home/data/repository/crypto_repository_impl.dart';
+import 'package:cryptos/app/features/home/domain/repository/crypto_repository.dart';
+import 'package:cryptos/app/features/home/domain/usecases/get_coins_usecase.dart';
+import 'package:cryptos/app/features/home/presentation/controller/home_controller.dart';
+import 'package:cryptos/core/network/http_adapter.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -8,8 +13,20 @@ void initDI() {
   _registerControllers();
 }
 
-void _registerRepositories() {}
+void _registerRepositories() {
+  sl.registerLazySingleton<CryptoRepository>(
+    () => CryptoRepositoryImpl(httpAdapter: HttpAdapter()),
+  );
+}
 
-void _registerUseCases() {}
+void _registerUseCases() {
+  sl.registerLazySingleton(() => GetCoinsUsecase(
+        repository: sl<CryptoRepository>(),
+      ));
+}
 
-void _registerControllers() {}
+void _registerControllers() {
+  sl.registerLazySingleton(() => HomeController(
+        getCoinsUsecase: sl<GetCoinsUsecase>(),
+      ));
+}
