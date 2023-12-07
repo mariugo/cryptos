e818e04f6864db24d427765955ff8c821e4f04e6
Mariugo Megiani <mariugomegiani@gmail.com>
2023-12-07 18:58:56 -0300

    Add home state management and coin tile widget

--- lib/app/features/home/presentation/screens/home_screen.dart
+++ lib/app/features/home/presentation/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:cryptos/app/features/home/presentation/controller/home_controller.dart';
+import 'package:cryptos/app/features/home/presentation/controller/home_state.dart';
+import 'package:cryptos/app/features/home/presentation/widgets/coin_tile.dart';
 import 'package:cryptos/core/base_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -22,6 +25,12 @@ class _HomeScreenState extends BaseState<HomeScreen, HomeController> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    controller.getCoins();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -36,7 +45,55 @@ class _HomeScreenState extends BaseState<HomeScreen, HomeController> {
           ),
         ),
       ),
-      body: const SafeArea(child: Column()),
+      body: BlocConsumer<HomeController, HomeState>(
+        listener: (context, state) {
+          state.status.matchAny(
+              any: () => hideLoader(),
+              loading: () => showLoader(),
+              error: () {
+                hideLoader();
+                showError(state.message!);
+              });
+        },
+        buildWhen: (previous, current) => current.status.matchAny(
+          any: () => false,
+          initial: () => true,
+          success: () => true,
+        ),
+        builder: (context, state) {
+          return state.status == HomeStatus.loading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : state.status == HomeStatus.error
+                  ? Center(
+                      child: Text(
+                        state.message!,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  : SafeArea(
+                      child: SingleChildScrollView(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: state.coins.length,
+                          itemBuilder: (context, index) {
+                            return CoinTile(
+                              imageUrl: state.coins[index].image,
+                              name: state.coins[index].name,
+                              price: state.coins[index].currentPrice,
+                              priceChangePercentage24h:
+                                  state.coins[index].priceChangePercentage24h,
+                            );
+                          },
+                        ),
+                      ),
+                    );
+        },
+      ),
     );
   }
 }
