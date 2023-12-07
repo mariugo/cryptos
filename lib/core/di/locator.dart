8c6aac76865a9f421bffcadf71c371b317afe34f
Mariugo Megiani <mariugomegiani@gmail.com>
2023-12-07 17:19:06 -0300

    Add dependency injection initialization code

---
+++ lib/core/di/locator.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void initDI() {
+  _registerRepositories();
+  _registerUseCases();
+  _registerControllers();
+}
+
+void _registerRepositories() {}
+
+void _registerUseCases() {}
+
+void _registerControllers() {}
