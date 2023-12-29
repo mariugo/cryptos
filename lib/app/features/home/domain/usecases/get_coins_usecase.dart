0cf451e809c965c5def45f6a66f0a8676e1f9335
Mariugo Megiani <mariugomegiani@gmail.com>
2023-12-29 09:47:09 -0300

    Refactor GetCoinsUsecase class to use arrow function syntax

--- lib/app/features/home/domain/usecases/get_coins_usecase.dart
+++ lib/app/features/home/domain/usecases/get_coins_usecase.dart
@@ -1,14 +1,12 @@
 import 'package:cryptos/app/features/home/data/models/cryto_coin_model.dart';
 import 'package:cryptos/app/features/home/domain/repository/crypto_repository.dart';
 
-class GetCoinsUsecase {
+final class GetCoinsUsecase {
   final CryptoRepository repository;
 
   const GetCoinsUsecase({
     required this.repository,
   });
 
-  Future<List<CryptoCoinModel>> call() async {
-    return await repository.getTopCoins();
-  }
+  Future<List<CryptoCoinModel>> call() async => await repository.getTopCoins();
 }
